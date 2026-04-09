acafc32f30e7e38e521c2bc5dab938cb4906e88e
Manas <nandanwarmanas@gmail.com>
2026-04-09 11:34:57 +0530

    Set up React Router for application navigation

--- main.jsx
+++ main.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import AddStudent from "./pages/AddStudent";
+import Students from "./pages/Students";
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "add-student", element: <AddStudent /> },
+      { path: "students", element: <Students /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
